1889c1550a51e407110b95203ae44a8048ccb8af
okt0312 <okt0312@naver.com>
2023-10-12 01:10:30 +0900

    요구사항 분석과 기본 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private Member member;
+
+    public Member getMember() {
+        return member;
+    }
+
+    private LocalDateTime localDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDateTime localDate) {
+        this.localDate = localDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
